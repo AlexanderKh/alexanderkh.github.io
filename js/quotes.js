a3ffe9e3ce9bcbf1900c5c5592ca7ce74d2176fa
Alexander Khomchenko <alexandr.khomchenko@gmail.com>
2020-02-22 14:56:59 +0200

    Links

---
+++ js/quotes.js
@@ -0,0 +1,9 @@
+const quotes = [
+  {
+    text: 'Pull me\nAnd then commit me\nAnd then just push me\nTo the production',
+  },
+  {
+    text: 'The meaning of life is to give life meaning',
+    author: 'Arjen Anthony Lucassen',
+  },
+];
